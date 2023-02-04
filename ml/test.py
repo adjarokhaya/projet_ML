635ad223a77ccc9099d1916e5f6f8a48e309c70d
rerwy <r.erwyn@gmail.com>
2023-02-04 20:45:23 +0100

    feat: bug fixed

--- ml/test.py
+++ ml/test.py
@@ -4,43 +4,33 @@ import numpy as np
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.layers import GlobalMaxPooling2D
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
-from numpy.linalg import norm
 from sklearn.neighbors import NearestNeighbors
 import cv2
 
-feature_list = np.array(pickle.load(open('embeddings.pkl', 'rb')))
+features = pickle.load(open("embeddings.pkl", "rb"))
+filenames = pickle.load(open("filenames.pkl", "rb"))
 
-print(np.array(feature_list).shape)
-filenames = pickle.load(open('filenames.pkl', 'rb'))
+# utilisation des modèle ResNet50 et GobalMaxPooling2D
+resnet50 = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+resnet50.trainable = False
+model = tensorflow.keras.Sequential([resnet50, GlobalMaxPooling2D()])
 
-model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
-model.trainable = False
-
-model = tensorflow.keras.Sequential([
-    model,
-    GlobalMaxPooling2D()
-])
-
-img = image.load_img('sample/ballon.jpg', target_size=(224, 224))
+#tensorflow
+img = image.load_img("sample/ballon.jpg", target_size=(224, 224))
 img_array = image.img_to_array(img)
 expanded_img_array = np.expand_dims(img_array, axis=0)
 preprocessed_img = preprocess_input(expanded_img_array)
 result = model.predict(preprocessed_img).flatten()
-normalized_result = result / norm(result)
 
-neighbors = NearestNeighbors(n_neighbors = 6, algorithm= 'brute',metric='euclidean')
-neighbors.fit(feature_list)
+# Normalisation pour poizat
+normalized_result = result / np.linalg.norm(result)
 
-distances, indices = neighbors.kneighbors([normalized_result])
-print(indices)
+# fonction de fit pour trouver les voisins les plus proches et enfin trouver les images
+neighbors = NearestNeighbors(n_neighbors=6, algorithm='brute', metric='euclidean')
+neighbors.fit(features)
+_, indices = neighbors.kneighbors([normalized_result])
 
-for file in indices[1:6]:
-    temp_img = cv2.imread(filenames[file])
-    cv2.imshow('output', cv2.resize(temp_img(512,512)))
+for file_index in indices[0][1:]:
+    temp_img = cv2.imread(filenames[file_index])
+    cv2.imshow("Output", cv2.resize(temp_img, (512, 512)))
     cv2.waitKey(0)
-
-
-
-
-
-
